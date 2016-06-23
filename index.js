5cbcc67a90ef2bffed730f5b9269b7d3de5a311c
fachry2 <hckraza@gmail.com>
2016-06-23 22:09:17 +0700

    make it better

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+	middleware = require('./middleware'),
+	app = express();
+	
+middleware(app);
 
-app.set('port', (process.env.PORT || 5000));
+app.use('/', express.static(__dirname + '/assets'));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.set('port', process.env.PORT || 1231);
+app.listen(app.get('port'), function(){
+	console.log('Server Running : ' + app.get('port'));
 });
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
